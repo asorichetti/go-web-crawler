119d508704cca9d71a03e39bbadc776b5ce88364
asorichetti <107128425+asorichetti@users.noreply.github.com>
2025-08-11 13:25:05 -0400

    FEAT: Created function for Extracting Links
    extractLinks function parses the HTML of the
    webpage that the Crawler is pointed at.

--- web_crawler.go
+++ web_crawler.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"net/url"
+	"os"
 	"sync"
 )
 
@@ -28,4 +30,92 @@ func NewCrawler(baseURL string, maxDepth int) (*Crawler, error) {
 		baseURL:  parsedURL,
 		results:  make(chan string, 100),
 	}, nil
+}
+
+// Crawl starts the crawling process
+func (c *Crawler) Crawl(startURL string, depth int){
+	defer c.wg.Done()
+
+	//Stop if max depth is reached
+	if depth > c.maxDepth{
+		return
+	}
+
+	// Normalize URL
+	parsedURL, err := url.Parse(startURL)
+	if err != nil{
+		fmt.Fprintf(os.Stderr, "Error parsing URL %s: %v\n", startURL, err)
+		return
+	}
+	if parsedURL.Host != c.baseURL.Host{
+		return //Skip external URLs
+	}
+	normalizedURL := parsedURL.String()
+
+	//Check if already visited
+	c.mutex.Lock()
+	if c.visited[normalizedURL]{
+		c.mutex.Unlock()
+		return
+	}
+	c.visited[normalizedURL] = true
+	c.mutex.Unlock()
+
+	// Fetch the page
+	resp, err := http.Get(normalizedURL)
+	if err != nil{
+		fmt.Fprintf(os.Stderr, "Error fetching %s: %v\n", normalizedURL, err)
+		return
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		fmt.Fprintf(os.Stderrm "Non-OK status for %s: %s\n", normalizedURL, resp.Status)
+		return
+	}
+
+	// Parse HTML and extract links
+	links, err := extractLinks(resp.Body, c.baseURL)
+	if err != nil{
+		fmt.Fprintf(os.Stderr, "Error parsing %s: %v\n", normalizedURL, err)
+		return
+	}
+
+	// Send URL to results channel
+	c.results <- normalizedURL
+
+	// Spawn goroutines for each link
+	for _, link := range links {
+		c.wg.Add(1)
+		go c.Crawl(link, depth+1)
+	}
+}
+
+// extractLinks parses HTML and returns valid links
+func extractLinks(body io.Reader, baseURL *url.URL) ([]string, error) {
+	var links []string
+	tokenizer := html.NewTokenizer(body)
+
+	for {
+		tt := tokenizer.Next()
+		switch tt{
+		case html.ErrorToken:
+			if tokenizer.Err() == io.EOF{
+				return
+			}
+			return nil, fmt.Errorf("error parsing HTML: %w", tokenizer.Err())
+		case html.StartTagToken, html.SelfClosingTagToken:
+			token := tokenizer.Token()
+			if tok.Date == "a"{
+				for _, attr := range token.Attr {
+					if attr.Key == "href"{
+						link, err := normalizeURL(attr.Val, baseURL)
+						if err == nil && link != ""{
+							links = append(links, link)
+						}
+					}
+				}
+			}
+		}
+	}
 }
